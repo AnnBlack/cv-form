8e09b423884da0a7ed898b8b28f02e5b21c4eccf
Анастасия Пуртова <anastasiya.purtova1@gmail.com>
2019-04-19 09:25:06 +0300

    added gitignore and bild folder

---
+++ build/precache-manifest.19fd38e64e38ebecf4e3a9abb9ad9f87.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6f6450769ce0b7b326cb9a6842aecdf",
+    "url": "/static/media/375E5F_1_0.a6f64507.woff"
+  },
+  {
+    "revision": "091d334ac39b18777a3a",
+    "url": "/static/css/main.716cc4ed.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2209ca143394f566fd61",
+    "url": "/static/js/2.2f0aa8d8.chunk.js"
+  },
+  {
+    "revision": "102d3585e9427b316a362a09d61d7aa4",
+    "url": "/static/media/375E5F_1_0.102d3585.eot"
+  },
+  {
+    "revision": "cf8aca26a0ca0e17d4e208b2d1e736fd",
+    "url": "/static/media/375E5F_0_0.cf8aca26.eot"
+  },
+  {
+    "revision": "c714814b39da2a4b11e5880fb482701f",
+    "url": "/static/media/375E5F_1_0.c714814b.woff2"
+  },
+  {
+    "revision": "c799eac34e88b5a8fab8be42220cdcc4",
+    "url": "/static/media/375E5F_0_0.c799eac3.woff2"
+  },
+  {
+    "revision": "091d334ac39b18777a3a",
+    "url": "/static/js/main.d01ea5f2.chunk.js"
+  },
+  {
+    "revision": "7b306326f17ec10acad7bb031f3c48f5",
+    "url": "/static/media/LabGrotesque-Light.7b306326.woff2"
+  },
+  {
+    "revision": "f3296507397c8c2f9d11dc52726ff683",
+    "url": "/static/media/375E5F_0_0.f3296507.woff"
+  },
+  {
+    "revision": "70f4100b30cce85b0c500a3abc4ad4e9",
+    "url": "/static/media/LabGrotesque-Light.70f4100b.woff"
+  },
+  {
+    "revision": "1f6d2da822e2841ba6e9c2dd520e9a70",
+    "url": "/static/media/LabGrotesque-Light.1f6d2da8.eot"
+  },
+  {
+    "revision": "685d27aff18a94aa31385fa74dce1606",
+    "url": "/static/media/375E5F_1_0.685d27af.ttf"
+  },
+  {
+    "revision": "63030e2134c8cf9b63f1aa0f689ce16f",
+    "url": "/static/media/375E5F_0_0.63030e21.ttf"
+  },
+  {
+    "revision": "ffd799d942b35c31067414308bb975be",
+    "url": "/index.html"
+  }
+];
